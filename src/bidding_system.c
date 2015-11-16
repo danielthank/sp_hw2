4e50a408190ff4fb161d3ed84adf43f068e22552
Daniel Chin <danielthank@gmail.com>
2015-11-16 18:18:07 +0800

    Add exec

--- src/bidding_system.c
+++ src/bidding_system.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include "utility.h"
 
 int main(int argc, char *argv[]) {
@@ -12,7 +14,17 @@ int main(int argc, char *argv[]) {
     for (int i=0; i<host_num; i++) {
         if (pipe(fd[i]) < 0)
             err_sys("pipe error");
+        if ((pid=fork()) < 0)
+            err_sys("fork error");
+        else if (pid == 0) {
+            char tmp[10];
+            sprintf(tmp, "%d", i+1);
+            if (execl("host", tmp, NULL) < 0)
+                err_sys("execl error");
+        }
     }
-    printf("%d %d\n", host_num, player_num);
+    while (pid = waitpid(-1, NULL, 0))
+        if (errno == ECHILD)
+            break;
     return 0;
 }
