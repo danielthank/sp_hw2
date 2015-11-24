b883efc7095d38e66e8b8388d8c0c856228f7ffa
Daniel Chin <danielthank@gmail.com>
2015-11-24 10:16:08 +0800

    Modify utility

--- src/utility.c
+++ src/utility.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "utility.h"
 
 void err_sys(char *str) {
     fprintf(stderr, "%s\n", str);
     exit(-1);
 }
+
+int compare_score(const void *a, const void *b) {
+    const Score *aa = (Score*)a;
+    const Score *bb = (Score*)b;
+    int tmp = bb->value - aa->value;
+    if (!tmp) return aa->id - bb->id;
+    return tmp;
+}
+
--- src/utility.h
+++ src/utility.h
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    int value;
+}Score;
+
 void err_sys(char*);
+
+int compare_score(const void*, const void*);
+
